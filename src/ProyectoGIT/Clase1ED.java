1f560f73a023ba31404e67c355205557119a63c4
PedroSarmiento12 <150545543+PedroSarmiento12@users.noreply.github.com>
2023-11-25 13:57:35 +0100

    Update Clase1ED.java
    Cambios desde GitHub

--- src/ProyectoGIT/Clase1ED.java
+++ src/ProyectoGIT/Clase1ED.java
@@ -4,8 +4,9 @@ public class Clase1ED {
 
 	public static void main(String[] args) {
 		String iniciales="PSG";
+		String cambiosGithub="Parece que todo ha funcionado";
 		
-		System.out.println("Hola te saluda "+iniciales);
+		System.out.println("Hola te saluda "+iniciales+" "+cambiosGithub);
 
 	}
 
