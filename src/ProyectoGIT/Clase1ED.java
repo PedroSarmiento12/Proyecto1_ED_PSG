677036f33c7f7fca2f25b687eb963d84ea54168a
bbdd2324 <bbdd2324@iescanaveral.canaveral.mostoles.ies>
2023-11-25 13:52:42 +0100

    Commit desde eclipse

---
+++ src/ProyectoGIT/Clase1ED.java
@@ -0,0 +1,12 @@
+package ProyectoGIT;
+
+public class Clase1ED {
+
+	public static void main(String[] args) {
+		String iniciales="PSG";
+		
+		System.out.println("Hola te saluda "+iniciales);
+
+	}
+
+}
